8f8703d40fd31a5797db8dbca7d7ca31ad10834a
童冀 <tongji_you@163.com>
2021-01-12 13:05:02 +0800

    Update conf.py
    add self message

--- conf.py
+++ conf.py
@@ -19,14 +19,14 @@ enable_jsdelivr = {
 }
 
 # 站点设置
-site_name = "我的个人博客"
+site_name = "daodao的个人博客"
 site_logo = "${static_prefix}logo.png"
-site_build_date = "2019-12-18T16:51+08:00"
-author = "熊猫小A"
-email = "hi@imalan.cn"
-author_homepage = "https://www.imalan.cn"
+site_build_date = "2020-01-12T16:51+08:00"
+author = "daodao"
+email = "daodao@gmail.com"
+author_homepage = "https://www.github.com"
 description = "只坚持一种正义。我的正义。"
-key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
+key_words = ['daodao', 'daodao', 'blog']
 language = 'zh-CN'
 external_links = [
     {
